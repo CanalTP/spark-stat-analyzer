95b4fada1b40aa7f9a0b992a4de780b4686810e0
David Quintanel <david.quintanel@canaltp.fr>
2017-03-13 13:55:03 +0100

    fix test

--- tests/integrations/error_stats_test.py
+++ tests/integrations/error_stats_test.py
@@ -10,17 +10,17 @@ class TestAnalyzeErrorStats(Mechanism):
         result = self.get_data(table_name='error_stats',
                                columns=['region_id', 'api', 'user_id', 'app_name',
                                         'is_internal_call', 'request_date', 'err_id',
-                                        'nb_req', 'nb_without_journey'])
+                                        'nb_req'])
 
         expected_results = [
-            ('fr-idf', 'v1.some_api', 42, 'my_app', 1, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1, None),
-            ('fr-foo', 'v1.some_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1, None),
-            ('fr-idf', 'v1.some_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 3, None),
-            ('fr-idf', 'v1.another_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1, None),
-            ('fr-idf', 'v1.some_api', 43, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1, None),
-            ('fr-idf', 'v1.some_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'another_error_id', 1, None),
-            ('fr-idf', 'v1.some_api', 42, 'another_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1, None),
-            ('fr-idf', 'v1.some_api', 42, 'my_app', 0, datetime(2013, 11, 16, 0, 0), 'some_error_id', 1, None),
+            ('fr-idf', 'v1.some_api', 42, 'my_app', 1, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1),
+            ('fr-foo', 'v1.some_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1),
+            ('fr-idf', 'v1.some_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 3),
+            ('fr-idf', 'v1.another_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1),
+            ('fr-idf', 'v1.some_api', 43, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1),
+            ('fr-idf', 'v1.some_api', 42, 'my_app', 0, datetime(2017, 1, 17, 0, 0), 'another_error_id', 1),
+            ('fr-idf', 'v1.some_api', 42, 'another_app', 0, datetime(2017, 1, 17, 0, 0), 'some_error_id', 1),
+            ('fr-idf', 'v1.some_api', 42, 'my_app', 0, datetime(2013, 11, 16, 0, 0), 'some_error_id', 1),
         ]
 
         assert same_list_tuple(result, expected_results)
