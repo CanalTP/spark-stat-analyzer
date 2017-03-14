d82358b430bde09aa558b9267b94f9a8310be611
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-03-14 12:07:49 +0100

    - split run to allow launch migration only in functionnal tests - allow disable progress bar

--- run_analyzer.sh
+++ run_analyzer.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
-pushd migrations
-
-echo 'Run migration database'
-
-PYTHONPATH=../ alembic upgrade head
-
-RESULT=$?
-
-if [ $RESULT -eq 0 ]
-then
-    echo "Migration database done"
-else
-    echo "Migration database failed"
-    exit 1
-fi
-
-popd
+./run_migrations.sh
 
 echo 'Run analyzer: '${TABLE}
 
 YESTERDAY_DATE=`date --date="yesterday" +%Y-%m-%d`
 START_DATE=${START_DATE:-$YESTERDAY_DATE}
 END_DATE=${END_DATE:-$YESTERDAY_DATE}
+PROGRESS_BAR=${PROGRESS_BAR:-true}
 
 ${SPARK_BIN}spark-submit \
     --py-files spark-stat-analyzer.zip \
-    --conf spark.ui.showConsoleProgress=true \
+    --conf spark.ui.showConsoleProgress=$PROGRESS_BAR \
     --master $MASTER_OPTION \
     --driver-java-options '-Duser.timezone=UTC' \
     manage.py \
