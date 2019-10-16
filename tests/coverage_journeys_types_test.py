6e7ef13ebd335e3c9396bf343c5f31de0d4cca41
Moomoo-Seukla <mourad.garnit@kisio.com>
2019-10-16 16:37:03 +0200

    back to normal

--- tests/coverage_journeys_types_test.py
+++ tests/coverage_journeys_types_test.py
@@ -6,7 +6,7 @@ import os
 pytestmark = pytest.mark.usefixtures("spark")
 path = os.getcwd() + "/tests/fixtures/coverage_journeys_types"
 
-def test_count_journeys_types_20170120(spark):
+def test_count_journeys_types(spark):
     start_date = date(2017, 1, 20)
     end_date = date(2017, 1, 20)
     expected_results = [
@@ -29,25 +29,3 @@ def test_count_journeys_types_20170120(spark):
 
     assert analyzer.get_log_analyzer_stats(datetime(2017, 3, 15, 15, 12)) == \
            "[OK] [2017-03-15 15:12:00] [2017-03-15 15:10:00] [CoverageJourneysTypes] [120]"
-
-def test_count_journeys_types_20190511(spark):
-    start_date = date(2019, 5, 11)
-    end_date = date(2019, 5, 11)
-    expected_results = [
-        (datetime.utcfromtimestamp(1557532800).date(), u'fr-foo', u'stop_point', u'..\\windows\\win.inistop_area', 1)
-    ]
-
-    analyzer = AnalyzeCoverageJourneysTypes(storage_path=path,
-                                            start_date=start_date,
-                                            end_date=end_date,
-                                            spark_session=spark,
-                                            database=None,
-                                            current_datetime=datetime(2019, 5, 11, 0, 0))
-    results = analyzer.get_data(rdd_mode=True)
-
-    assert len(results) == len(expected_results)
-    for result in results:
-        assert result in expected_results
-
-    assert analyzer.get_log_analyzer_stats(datetime(2019, 5, 11, 0, 0)) == \
-           "[OK] [2019-05-11 00:00:00] [2019-05-11 00:00:00] [CoverageJourneysTypes] [0]"
--- tests/integrations/coverage_journeys_types_test.py
+++ tests/integrations/coverage_journeys_types_test.py
@@ -16,9 +16,7 @@ class TestAnalyzeCoverageJourneysTypes(Mechanism):
             (datetime(2017, 1, 20, 0, 0), 'fr-foo', 'stop_area', 'poi', 2)
         ]
 
-        for expected_result in expected_results:
-            assert expected_result in result
-            
+        assert result == expected_results
         assert self.partitionned_table_exists('coverage_journeys_types_y2017m01')
 
     def test_coverage_journeys_types_with_null_character(self):
