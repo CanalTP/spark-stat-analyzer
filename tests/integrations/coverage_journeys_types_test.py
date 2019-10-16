bb72a5031b061b3e59bf3b1f08eff83f88af834a
Pascal VanDerSwalmen <pascal.vanderswalmen@kisio.com>
2019-10-16 16:27:11 +0200

    clean test (do not select all dates)

--- tests/integrations/coverage_journeys_types_test.py
+++ tests/integrations/coverage_journeys_types_test.py
@@ -7,7 +7,9 @@ class TestAnalyzeCoverageJourneysTypes(Mechanism):
     def test_coverage_journeys_types(self):
         self.launch(analyzer='coverage_journeys_types', start_date='2017-01-20', end_date='2017-01-20')
         result = self.get_data(table_name='coverage_journeys_types',
-                               columns=['request_date', 'region_id', 'from_type', 'to_type', 'nb'])
+                               columns=['request_date', 'region_id', 'from_type', 'to_type', 'nb'],
+                               start_date='2017-01-20',
+                               end_date='2017-01-20')
         expected_results = [
             (datetime(2017, 1, 20, 0, 0), 'fr-foo', 'address', 'poi', 1),
             (datetime(2017, 1, 20, 0, 0), 'fr-foo', 'stop_point', 'stop_area', 3),
